a5c78a759d7be2e83ecd3e45ec5f2adf69d9c283
bellatoris <bellatoris@snu.ac.kr>
2016-10-22 18:53:18 +0900

    [MyScala] Second Course start

--- LinuxFirstScala/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "LinuxFirstScala"
-
-version := "1.0"
-
-scalaVersion := "2.11.8"
-    
--- LinuxFirstScala/src/main/scala/ExampleApp.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by doogiemin on 16. 9. 25.
-  */
-object ExampleApp extends App {
-  println("Hello, world")
-}
